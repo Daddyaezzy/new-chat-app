b9c2f9f55f1c363206b407643084e131d80c3d1b
daddyaezzy <pulumbu11@gmail.com>
2023-06-13 17:00:04 +0100

    up

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 
